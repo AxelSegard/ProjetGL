428400cecdb56218eeefe6d96a9ee5837b1a5b90
Axel Segard <yoshibarox@gmail.com>
2017-05-11 13:18:43 +0200

    3nd commit

---
+++ build/CMakeFiles/chifoumi.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chifoumi.out.dir/src/chifoumi.cpp.o"
+  "chifoumi.out.pdb"
+  "chifoumi.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chifoumi.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
